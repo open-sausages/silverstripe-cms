447f9ac6405a2748b6a61c897bbec8a506a5c958
Damian Mooyman <damian@silverstripe.com>
2016-03-29 11:39:12 +1300

    API Support new DataObject::can() signature

--- code/model/SiteTree.php
+++ code/model/SiteTree.php
@@ -760,6 +760,7 @@ class SiteTree extends DataObject implements PermissionProvider,i18nEntityProvid
 	 *
 	 * @param string $perm The permission to be checked, such as 'View'
 	 * @param Member $member The member whose permissions need checking. Defaults to the currently logged in user.
+	 * @param array $context Context argument for canCreate()
 	 * @return bool True if the the member is allowed to do the given action
 	 */
 	public function can($perm, $member = null, $context = array()) {
